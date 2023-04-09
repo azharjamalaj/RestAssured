0e9a2185cab1869d33eb10c9dd378c3b7fe76e7c
azharjamalaj <87015215+azharjamalaj@users.noreply.github.com>
2023-04-09 13:20:42 +0530

    How to use filter
    Filter example, how to extract json response based on filters

---
+++ src/test/java/restfulBooker/jsonPath/JsonExampleWIthFilter.java
@@ -0,0 +1,28 @@
+package restfulBooker.jsonPath;
+
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.List;
+
+public class JsonExampleWIthFilter {
+    @Test
+    public void jsonExampleUsingFilter()
+    {
+        String filePath=System.getProperty("user.dir")+"\\src\\test\\java\\restfulBooker\\jsonPath\\payload.json";
+        System.out.println(filePath);
+        File file= new File(filePath);
+        JsonPath json = new JsonPath(file);
+
+        System.out.println(json.getString("email"));
+        List<String> listOfEmail = json.getList("email");
+
+        System.out.println(listOfEmail);
+
+       List<String> list_firstNameFilter =json.getList("findAll{it.first_name=='Matin' ||it.last_name=='Matin'}");
+        System.out.println(list_firstNameFilter);
+
+    }
+}
+ 
